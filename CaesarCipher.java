47f68e401c4795ee1ce092d06765bd57c3572506
bLeohr <benjamin.leohr@stu.daviess.kyschools.us>
2025-02-21 03:41:21 +0000

    caeserstuff

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -6,32 +6,80 @@
  * @version 1.00 2025/2/20
  */
 
+import java.util.Arrays;
 
 public class CaesarCipher {
 
-	private char[] alphabet;
+	private char[] ALPHABET = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        
 	private char[] shifted;
 	private int shift;
 
     public CaesarCipher() {
-
+        shift = 0;
+        shifted = new char[26];
+        shifter(0);
     }
 
     public CaesarCipher(int num){
 
+        shift = num;        
+        shifted = new char[26];
+        shifter(shift);
     }
 
     public String encrypt(String message){
+        String returnString="";
+        boolean isItThere = false;
 
+        for(int i=0; i<message.length(); i++){
+            for(int letter=0; letter<ALPHABET.length; letter++)
+                if(message.charAt(i)==(ALPHABET[letter])){ 
+                    returnString+=(shifted[letter]);
+                isItThere=true;}
+        if(!isItThere)
+            returnString+=message.charAt(i);
+
+        isItThere = false;
+         }
+        return returnString;
     }
 
     public String decrypt(String message){
+        String returnString = "";
+        boolean isItThere = false;
 
+        for(int i=0; i<message.length(); i++){
+            for(int letter=0; letter<shifted.length; letter++){
+                if(message.charAt(i)==(shifted[letter])){
+                    returnString+=(ALPHABET[letter]);
+                    isItThere=true;}
+            }
+            if(!isItThere)
+                returnString+=message.charAt(i);
+
+            isItThere=false;
+        }
+        return returnString;
     }
 
     public void shifter(int num){
+        for(int i=0; i<ALPHABET.length; i++)
+            shifted[(i+num)%26]=ALPHABET[i];
+    
+        /*{
+        if(i-num<0)
+            shifted[i]=ALPHABET[(i-num)+shifted.length];
 
+        else
+            shifted[i]=ALPHABET[i-num];
+        }*/
     }
 
+    public String toString(){
+        System.out.println(Arrays.toString(ALPHABET));
+        System.out.println(Arrays.toString(shifted));
+        return "";
+    }
 
 }
--- G19.java
+++ G19.java
@@ -12,12 +12,21 @@ public class G19 {
     public static void main(String[] args) {
 
     	CaesarCipher cipher = new CaesarCipher(4);
+		System.out.println(cipher);
 
     	System.out.println(cipher.encrypt("hello world!"));
     	System.out.println(cipher.decrypt("ukq zez ep!"));
 
     	//Create a new cipher using the shifter(#) method
     	//Encrypt and decrypt a message with your new cipher
+
+		CaesarCipher cipher2 = new CaesarCipher(6);
+		System.out.println();
+		System.out.println(cipher2);
+
+    	System.out.println(cipher2.encrypt("this assignment was so invigorating!!!"));
+		System.out.println(cipher2.decrypt("domn ecxxcha :_("));
+
 
     }
 
